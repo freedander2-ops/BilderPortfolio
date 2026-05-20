9a7a8e3020a6c35266b0571c670ef3022ede9680
NullWeaver <254006109+freedander2-ops@users.noreply.github.com>
2026-05-21 02:02:39 +0300

    Delete scripts/verification_script.js

--- scripts/verification_script.js
+++
@@ -1,9 +0,0 @@
-const { chromium } = require('playwright');
-(async () => {
-  const browser = await chromium.launch();
-  const page = await browser.newPage();
-  await page.goto('file://' + process.cwd() + '/index.html');
-  await page.waitForTimeout(1000);
-  await page.screenshot({ path: 'verification/engineering_check.png', fullPage: true });
-  await browser.close();
-})();
